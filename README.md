80c8886258b969dd7f3a360f599a690947fbe4d8
Rojan Jose <rojanjose@gmail.com>
2019-03-24 13:22:48 +0000

    Inital changes

---
+++ README.md
@@ -0,0 +1,2 @@
+
+Scripts to provision IKS workshop environment.
---
+++ scripts/configureClusters.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Script to delete clusters in a batch
+
+CLUSTER_RANGE=$1;
+CSTART=$(cut -d'-' -f1 <<< $CLUSTER_RANGE);
+CEND="$(cut -d'-' -f2 <<< $CLUSTER_RANGE)";
+
+for num in `seq -f "%03g" $CSTART $CEND`; 
+do 
+
+CLUSTER_NAME="user$num-cluster"
+echo "Configuring cluster $CLUSTER_NAME ..."
+
+eval $(ibmcloud ks cluster-config --cluster $CLUSTER_NAME --export)
+kubectl apply -f https://raw.githubusercontent.com/IBM-Cloud/kube-samples/master/rbac/serviceaccount-tiller.yaml
+helm init --service-account tiller
+
+# set up istio
+yes | ibmcloud ks cluster-addon-enable istio-extras --cluster $CLUSTER_NAME
+
+done
+
