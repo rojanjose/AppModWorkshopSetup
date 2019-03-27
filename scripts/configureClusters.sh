1eb5021c7d5b61a98aaee67c69bcfc7cbe97cc01
Matt Langbehn <matthew.langbehn@gmail.com>
2019-03-27 14:22:52 -0400

    Update scripts for best practices
    Signed-off-by: Matt Langbehn <matthew.langbehn@gmail.com>

--- scripts/configureClusters.sh
+++ scripts/configureClusters.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
-#Script to delete clusters in a batch
 
 CLUSTER_RANGE=$1;
-CSTART=$(cut -d'-' -f1 <<< $CLUSTER_RANGE);
-CEND="$(cut -d'-' -f2 <<< $CLUSTER_RANGE)";
+CSTART=$(cut -d'-' -f1 <<< "$CLUSTER_RANGE");
+CEND=$(cut -d'-' -f2 <<< "$CLUSTER_RANGE");
 
-for num in `seq -f "%03g" $CSTART $CEND`; 
-do 
-
+for num in $(seq -f "%03g" "$CSTART" "$CEND"); do
 CLUSTER_NAME="user$num-cluster"
 echo "Configuring cluster $CLUSTER_NAME ..."
 
-eval $(ibmcloud ks cluster-config --cluster $CLUSTER_NAME --export)
+eval $(ibmcloud ks cluster-config --cluster "$CLUSTER_NAME" --export)
 kubectl apply -f https://raw.githubusercontent.com/IBM-Cloud/kube-samples/master/rbac/serviceaccount-tiller.yaml
 helm init --service-account tiller
 
 # set up istio
-yes | ibmcloud ks cluster-addon-enable istio-extras --cluster $CLUSTER_NAME
-
+yes | ibmcloud ks cluster-addon-enable istio-extras --cluster "$CLUSTER_NAME"
 done
-
