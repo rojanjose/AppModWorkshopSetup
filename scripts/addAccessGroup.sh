f2bcde7288e405e3e73cfb5ab466a3e3764903f0
Matt Langbehn <matthew.langbehn@gmail.com>
2019-03-29 12:30:30 -0500

    Add POSIX style help messages
    Previously, users had to read the scripts to understand how to run them.
    This was a party foul.
    
    Now, all scripts print a help message with -h, --help, or unexpected
    flags.
    
    Signed-off-by: Matt Langbehn <matthew.langbehn@gmail.com>

--- scripts/addAccessGroup.sh
+++ scripts/addAccessGroup.sh
@@ -3,6 +3,21 @@
 ACCESS_GROUP="${ACCESS_GROUP:-AppMod}"
 USER_LIST="${USER_LIST:-email.txt}"
 
+print_help(){
+cat << EOF
+Usage: $0 [OPTIONS]...
+
+Add users from a file to an access group.
+
+Mandatory arguments to long options are mandatory for short options too.
+  -g=, --access-group=STRING       add users to STRING access group
+                                       defaults to App-Mod
+  -u=,  --user-list=FILE           FILE where users email addresses are stored;
+                                       defaults to email.txt
+  -h   --help                      display this help and exit
+
+EOF
+}
 
 for OPT in "$@"; do
     case "$OPT" in
@@ -12,8 +27,13 @@ for OPT in "$@"; do
         -u=*|--user-list=*)
             USER_LIST="${OPT#*=}"
             ;;
+        -h|--help)
+            print_help
+            exit 0
+            ;;
         *)
             echo "Unexpected flag $OPT"
+            print_help
             exit 2
             ;;
     esac
--- scripts/configureClusters.sh
+++ scripts/configureClusters.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
 
-CLUSTER_RANGE="${CLUSTER_RANGE:-1-50}"
+print_help(){
+cat << EOF
+Usage: $0 [OPTIONS]...
+
+Configure Clusters.
+
+Mandatory arguments to long options are mandatory for short options too.
+  -r=,  --cluster-range=NUM1-NUM2    range from NUM1 to NUM2 for cluster numbers;
+                                         defaults to 1-50
+  -h   --help                        display this help and exit
+
+EOF
+}
 
 for OPT in "$@"; do
     case "$OPT" in
         -r=*|--cluster-range=*)
             CLUSTER_RANGE="${OPT#*=}"
             ;;
+        -h|--help)
+            print_help
+            exit 0
+            ;;
         *)
             echo "Unexpected flag $OPT"
+            print_help
             exit 2
             ;;
     esac
 done
+
+if [[ -z $CLUSTER_RANGE ]]; then
+    echo "Error: cluster range required."
+    print_help
+    exit 3
+fi
 
 CSTART=$(cut -d'-' -f1 <<< "$CLUSTER_RANGE");
 CEND=$(cut -d'-' -f2 <<< "$CLUSTER_RANGE");
